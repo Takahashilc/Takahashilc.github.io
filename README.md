670d409c45395c2d53b867ad9b3751e5b01288a1
takahashilc <ltakahashi0896@gmail.com>
2022-09-20 15:47:42 -0300

    Portfólio Inicial

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Takahashilc.github.io
+# PROJETO 1
+## Menu Hamburguer - Teste DIO 1
+
+### Primeiro projeto seguindo as aulas fornecidas pela DIO.me, primeiro contato com a ferramenta de HTML e CSS.
---
+++ js/main.js
@@ -0,0 +1,12 @@
+var botaoLinkedin = document.querySelector('#linkedin-card');
+var botaoGithub = document.querySelector('#github-card');
+
+botaoLinkedin.addEventListener('click', function(){
+    window.open('https://www.linkedin.com/in/leandrotakahashidacosta/');
+})
+
+botaoGithub.addEventListener('click', function(){
+    window.open('https://github.com/Takahashilc');
+})
+
+
